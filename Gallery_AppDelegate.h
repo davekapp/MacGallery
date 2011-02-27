c846c80c795c240b62d4e339aca11b5055900269
Dave Kapp <davekapp@gmail.com>
2011-02-27 15:41:03 -0700

    initial commit

---
+++ Gallery_AppDelegate.h
@@ -0,0 +1,28 @@
+//
+//  Gallery_AppDelegate.h
+//  Gallery
+//
+//  Created by David Kapp on 2/25/11.
+//  Copyright __MyCompanyName__ 2011 . All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface Gallery_AppDelegate : NSObject 
+{
+    NSWindow *window;
+    
+    NSPersistentStoreCoordinator *persistentStoreCoordinator;
+    NSManagedObjectModel *managedObjectModel;
+    NSManagedObjectContext *managedObjectContext;
+}
+
+@property (nonatomic, retain) IBOutlet NSWindow *window;
+
+@property (nonatomic, retain, readonly) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+@property (nonatomic, retain, readonly) NSManagedObjectModel *managedObjectModel;
+@property (nonatomic, retain, readonly) NSManagedObjectContext *managedObjectContext;
+
+- (IBAction)saveAction:sender;
+
+@end
---
+++ main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  Gallery
+//
+//  Created by David Kapp on 2/25/11.
+//  Copyright __MyCompanyName__ 2011. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, char *argv[])
+{
+    return NSApplicationMain(argc,  (const char **) argv);
+}
